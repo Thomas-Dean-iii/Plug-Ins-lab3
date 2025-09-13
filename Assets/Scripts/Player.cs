6e6561c17d735a364f6d34a98bec464457035cd7
misakicb <nickt00012@gmail.com>
2025-09-13 17:43:55 -0400

    Added player movement
    Player is able to move from one side to the other.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement2D : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Awake()
     {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+       
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        
+        if (moveInput != 0)
+            transform.localScale = new Vector3(Mathf.Sign(moveInput), 1, 1);
+
+        
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDrawGizmosSelected()
     {
-        
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
     }
 }
