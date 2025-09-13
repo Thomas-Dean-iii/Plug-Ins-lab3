2d8a10a562830c89257971614feea9c66b7520fb
Tom-Dean <Thomas.dean.iii1002@gmail.com>
2025-09-12 20:13:28 -0400

    added rotating circles

--- Assets/Scripts/Circles.cs
+++ Assets/Scripts/Circles.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Circles : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform Target;
+    public float Distance;
+    private float speed;
+    private float angle;
+
+    
+
     void Start()
     {
-        
+        Target = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        speed = Distance * .5f;
+        angle += speed * Time.deltaTime;
+
+        float x = Target.position.x + Mathf.Cos(angle) * Distance;
+        float y = Target.position.y + Mathf.Sin(angle) * Distance;
+
+        transform.position = new Vector2(x, y);
     }
 }
